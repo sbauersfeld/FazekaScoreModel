26a2401e79cc8a3befe66d22e490cb876eb963ab
Scott <sbauersfeld@g.ucla.edu>
2019-05-02 15:23:36 -0700

    added data

--- project.py
+++ project.py
@@ -2,3 +2,7 @@ import nibabel as nib
 import numpy as np
 import pydicom
 
+dcmImage = pydicom.dcmread('Data/101_8_30_15/IM-0065-0001.dcm')
+dcm_scan = dcmImage.pixel_array
+print('The shape is:')
+print(np.shape(dcm_scan))
