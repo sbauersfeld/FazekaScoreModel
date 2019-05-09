6851893d16deeffa2f11494ad23dfd81fed82720
Scott <sbauersfeld@g.ucla.edu>
2019-05-09 15:15:42 -0700

    can load dcm or mat files

--- project.py
+++ project.py
@@ -4,9 +4,13 @@ import pydicom
 import matplotlib.pyplot as plt
 import os
 from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from scipy.io import loadmat
 
 ALL_DATA = 'Data' # The root directory for our data
-NUM_PATIENTS = 8 # The number of patients we want to use
+PREPROCESSED_DATA = 'Data/Preprocessed'
+ORIGINAL_DATA = 'Data/Original'
+NUM_PATIENTS = 1 # The number of patients we want to use
 
 def load_patient_scans(data_path):
     """
@@ -26,9 +30,29 @@ def load_patient_scans(data_path):
     dcm_scans = [image.pixel_array for image in dcmImages] # get the pixel array from each dicom image
     return dcm_scans
 
-def get_train_test_data():
+def load_processed_data(data_path):
+    """
+    Load patient data.
+        
+    Parameters
+    --------------------
+        data path    -- string formatted path to the folder containing the patient data
+    
+    Returns
+    --------------------
+        data        -- numpy array pixel arrays with shape (n,x,y), 
+                        one pixel array for each of the n patient images
+    """
+    data = loadmat(data_path)['stack1'] # load the data TODO: change this to 'stack'
+    return np.moveaxis(data, -1, 0) # swap axes for consistency with original data
+
+def get_data(data_type):
     """
     Get training and test data and labels.
+
+    Parameters
+    --------------------
+    data type   -- string of either "dcm" or "mat" specifiying which data to load
 
     Returns
     --------------------
@@ -37,27 +61,41 @@ def get_train_test_data():
     train output    -- numpy array with shape (n,) each entry is the labeled fazeka score for the nth patient
     test output     -- same as train output but with testing data
     """
+    if data_type == 'dcm': # choose which data to load/use
+        data_path = ORIGINAL_DATA
+        load_func = load_patient_scans
+    elif data_type == 'mat':
+        data_path = PREPROCESSED_DATA
+        load_func = load_processed_data
+    else: 
+        raise Exception("Input to get_data invalid. It must be either \'dcm\' or \'mat\'. It was: {}".format(data_type))
+
     all_data_input = np.empty((NUM_PATIENTS), dtype=list)
     all_data_labels = np.zeros((NUM_PATIENTS)) #TODO: Fill in fazeka scores
     i=0
-    for s in os.listdir(ALL_DATA):
+    for s in os.listdir(data_path):
         if i >= NUM_PATIENTS: # use num_patients to limit the amount of data used
             break
 
-        all_data_input[i] = load_patient_scans(ALL_DATA + '/' + s) # load the data for each patient
+        all_data_input[i] = load_func(data_path + '/' + s) # load the data for each patient
         i += 1
 
-    return train_test_split(all_data_input, all_data_labels, test_size=0.2) # split data into training and testing set
+    train_input, test_input, train_output, test_output = train_test_split(all_data_input, all_data_labels, test_size=0.0) # split data into training and testing set
+    train_output = to_categorical(train_output, num_classes=12)
+    test_output = to_categorical(test_output, num_classes=12)
+    return train_input, test_input, train_output, test_output
 
 def main():
-    train_input, test_input, train_output, test_output = get_train_test_data()
+    train_input, test_input, train_output, test_output = get_data("mat")
     print(np.shape(train_input))
+    print(np.shape(test_input))
     print(np.shape(train_input[0]))
-    # for scan in train_input[0]:
-    #     print('The shape is:')
-    #     print(np.shape(scan))
-    #     plt.imshow(scan, cmap='gray')
-    #     plt.show()
+    print(np.shape(train_output))
+    for scan in train_input[0]:
+        print('The shape is:')
+        print(np.shape(scan))
+        plt.imshow(scan, cmap='gray')
+        plt.show()
 
 if __name__ == '__main__':
     main()
