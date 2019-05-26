2927f0d5b738e865de294f2bfd64b160164281c9
serenekamal <50219896+serenekamal@users.noreply.github.com>
2019-05-25 19:29:24 -0700

    added corresponding labels

--- project.py
+++ project.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 def main():
     data = util.load_processed_data(util.PREPROCESSED_DATA) # this is how we can load the data for conv nets
     print(np.shape(data))
+    peri_vals = util.load_patient_labels(util.LABEL_DATA,"","peri") #this outputs the average periventricular Fazekas score
+    deep_vals = util.load_patient_labels(util.LABEL_DATA,"","deep") #this outputs the average deep Fazekas score
+    
     util.multi_slice_subplot(data[1])
     util.multi_slice_subplot(data[12])
     util.multi_slice_subplot(data[40])
