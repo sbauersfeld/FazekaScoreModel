791fbba817fc7e7af8383e2a7d7aa16d0d3e9989
Scott <sbauersfeld@g.ucla.edu>
2019-05-29 18:49:25 -0700

    can train and test initial model

--- project.py
+++ project.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras
 
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+
 # TODO: normalize images
 # def normalize(data):
 # consider tf.image.per_image_standardization(image)
@@ -48,6 +51,28 @@ def main():
     fazeka_model = keras.Sequential([base_model, global_average_layer, prediction_layer])
     fazeka_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
     fazeka_model.summary()
+
+
+    all_data_input = np.zeros((n*m,y,x,3), dtype = np.float32) # make each scan its own input
+    all_data_labels = np.zeros((n*m))
+    for i in range(n):
+        all_data_input[i*m:(i+1)*m] = np.repeat(data[i,:,:,:,np.newaxis], 3, axis=3) # replicate greyscale images to make 3 channels
+        all_data_labels[i*m:(i+1)*m] = np.repeat(peri_vals[i], m)
+
+    print(all_data_input.shape)
+    print(all_data_labels.shape)
+    train_input, test_input, train_output, test_output = train_test_split(all_data_input, all_data_labels, test_size=0.2)
+    train_output = to_categorical(train_output, num_classes=4)
+    test_output = to_categorical(test_output, num_classes=4)
+
+    batch_size = 10
+    epochs = 20
+    fazeka_train = fazeka_model.fit(train_input, train_output, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(test_input, test_output))
+    test_eval = fazeka_model.evaluate(test_input, test_output, verbose=0)
+    print('Test loss:', test_eval[0])
+    print('Test accuracy:', test_eval[1]) 
+
+    fazeka_model.save('test1.h5')
 
 if __name__ == '__main__':
     main()
